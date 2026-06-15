34d8b679b7ab88a646114d0f36f585f6453d5761
Rey Enmanuel Marte Picherdo <email>
2026-06-14 21:38:27 -0400

    Creacion de los metodos restar, multiplicar y dividir

--- src/Ejercicio4/Calculadora.java
+++ src/Ejercicio4/Calculadora.java
@@ -7,7 +7,8 @@ public class Calculadora {
     Scanner sc = new Scanner(System.in);
 
     public void Sumar() {
-        int suma = 0;
+        try{
+             int suma = 0;
 
         System.out.println("Cuantos numeros deseas sumar: ");
         int cantidad = sc.nextInt();
@@ -15,18 +16,88 @@ public class Calculadora {
 
         for(int i = 0; i <= cantidad; i++){
             System.out.println("Ingrese numero #" + (i + 1));
-            try{
-                int numero = sc.nextInt();
-                sc.nextLine();
-                suma += numero;
-            }catch(InputMismatchException e) {
-                System.out.println("Error: No se puede realizar la operacion con letras");
-            } finally{
-                System.out.println("Proceso Finalizado...");
+            int numero = sc.nextInt();
+            sc.nextLine();
+            suma += numero;
+        }
+        System.out.println("Suma Total = " + suma);
+        }catch(InputMismatchException e) {
+            System.out.println("Error: No se puede realizar la operacion con letras");
+        } finally{
+            System.out.println("Proceso Finalizado...");
+        }
+    }
+
+    public void Restar() {
+       try{
+        System.out.println("Ingrese la cantidad de numeros que desea restar");
+        int resta = 0;
+        int cantidad = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 0; i <= cantidad; i++){
+            System.out.println("Ingrese el numero que desea restar");
+            int numero = sc.nextInt();
+            sc.nextLine();
+            if(i == 0){
+                resta = numero;
             }
-        
+            else {
+                resta -= numero;
+            }
         }
 
-        System.out.println("Suma Total = " + suma);
+        System.out.println("Total restante = " + resta);
+       }catch(InputMismatchException e) {
+        System.out.println("Error: No se puede realizar la operacion con letras. Ingrese un numero valido");
+       } finally{
+        System.out.println("Proceso Finalizado...");
+       }
+    }
+
+    public void Multiplicar() {
+        try{
+            System.out.print("-");
+            int numero1 = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print(" * ");
+
+            int numero2 = sc.nextInt();
+            sc.nextLine();
+
+            int producto = numero1 * numero2;
+
+            System.out.print("= " + producto);
+
+        }catch(InputMismatchException e){
+            System.out.println("Error: No se puede realizar la operacion. Ingrese un numero valido");
+        }finally{
+            System.out.println("Proceso Finalizado...");
+        }
+    }
+
+    public void Dividir() {
+        try{
+            System.out.print("-");
+            int numero1 = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print(" * ");
+
+            int numero2 = sc.nextInt();
+            sc.nextLine();
+
+            int producto = numero1 * numero2;
+
+            System.out.print("= " + producto);
+
+        }catch(InputMismatchException e){
+            System.out.println("Error: No se puede realizar la operacion. Ingrese un numero valido");
+        }catch(ArithmeticException e){
+            System.out.println("Error: No se puede dividir entre 0");
+        }finally{
+            System.out.println("Proceso Finalizado...");
+        }
     }
 }
