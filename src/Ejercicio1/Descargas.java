c1144e8342592a66e924911dde91c378cd9bfbd2
Rey Enmanuel Marte Picherdo <email>
2026-06-15 15:14:53 -0400

    Creacion de la clase descargas y el ciclo de descargas

--- src/Ejercicio1/Descargas.java
+++ src/Ejercicio1/Descargas.java
@@ -9,7 +9,20 @@ public class Descargas extends Thread{
 
     @Override
     public void run(){
-        System.out.println("Descargando " + NombreArchivo);
+        System.out.println("Descargando " + NombreArchivo + "...");
+
+        for(int i = 0; i < 100; i += 5){
+            System.out.println(NombreArchivo + " " + i + "%");
+
+            try{
+                Thread.sleep(2000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }   
+        }
+
+        System.out.println("...");
+        System.out.println(NombreArchivo + " Completado");
         
     }
     
