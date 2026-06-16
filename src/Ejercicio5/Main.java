8b71924b170ee9e2b09e23cd7007bf690d24388d
Rey Enmanuel Marte Picherdo <email>
2026-06-15 23:51:52 -0400

    Creacion del menu

--- src/Ejercicio5/Main.java
+++ src/Ejercicio5/Main.java
@@ -1,7 +1,37 @@
 package src.Ejercicio5;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        
+        int op = 0;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("=====================================");
+            System.out.println("    .::REGISTRO DE ACTIVIDADES::.    ");
+            System.out.println("=====================================");
+            System.out.println("-------------------------------------");
+            System.out.println("1.  Registrar Actividades");
+            System.out.println("2.  Mostrar Actividades");
+            System.out.println("3.  Salir");
+            System.out.println("-------------------------------------");
+
+            System.out.print("Ingrese la opcion deseada: ");
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch(op) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+            }
+        }while(op != 3);
     }
 }
